a22dc9c5a5d538e0adb9df347cf1bdfa36e0d68f
Uduak Umanah <eyakenojnr@gmail.com>
2025-01-31 13:58:19 +0100

    Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -44,7 +44,8 @@ class Auth:
         """
         if request is None:
             return None
-        return header
+        #return header
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
