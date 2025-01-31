5a0ac7c09dd19679a4c5f868c7b9e08880ea9641
Uduak Umanah <eyakenojnr@gmail.com>
2025-01-31 06:25:01 +0100

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,33 +20,31 @@ class Auth:
         Return:
             - True if path is not in excluded_paths, else False
         """
-        #if path is None:
-         #   return True
-       # elif excluded_paths is None or excluded_paths == []:
-        #    return True
-       # elif path in excluded_paths:
-        #    return False
-       # else:
-        #    for i in excluded_paths:
-         #       if i.startswith(path):
-         #           return False
-          #      if path.startswith(i):
-           #         return False
-            #    if i[-1] == "*":
-             #       if path.startswith(i[:-1]):
-              #          return False
-        #return True
-        return False
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
 
 
     def authorization_header(self, request=None) -> str:
         """
         Returns the authentication header from a request object
         """
-        #if request is None:
-         #   return None
-       # return header
-       return None
+        if request is None:
+            return None
+        return header
 
 
     def current_user(self, request=None) -> TypeVar('User'):
