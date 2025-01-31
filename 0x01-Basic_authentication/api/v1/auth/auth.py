02019fe01cb7589b44cb265bf80059f5e6e0ee7e
Uduak Umanah <eyakenojnr@gmail.com>
2025-01-31 06:09:19 +0100

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+Auth class definition
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Manage API authenticatiion
+    """    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if a path requires authentication
+        Args:
+            - path(str): Url path to be checked
+            - excluded_paths(List of str): List of paths that do not require
+              authentication
+        Return:
+            - True if path is not in excluded_paths, else False
+        """
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
+
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns the authentication header from a request object
+        """
+        if request is None:
+            return None
+        return header
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns a User instance from information from a request object
+        """
+        return None
