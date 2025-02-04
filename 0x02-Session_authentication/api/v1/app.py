143c094f991632b12202b5cb669902fde9cfc76a
Uduak Umanah <eyakenojnr@gmail.com>
2025-02-04 01:26:39 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,19 +38,24 @@ def bef_req():
     """
     if auth is None:
         pass
+    
     excluded_paths = [
         "/api/v1/status/",
         "/api/v1/unauthorized/",
         '/api/v1/forbidden/',
         "/api/v1/auth_session/login/"
     ]
+    
     if not auth.require_auth(request.path, excluded_paths):
         return
-    #cookie = auth.session_cookie(request)
-    if auth.authorization_header(request) is None:
+    
+    cookie = auth.session_cookie(request)
+    
+    if auth.authorization_header(request) is None and cookie is None:
         abort(401, description="Unauthorized")
     if auth.current_user(request) is None:
         abort(403, description="Forbidden")
+    
     # Set current_user after authorization check
     setattr(request, "current_user", auth.current_user(request))
 
